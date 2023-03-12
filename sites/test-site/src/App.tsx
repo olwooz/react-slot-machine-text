557fb08dda9be7c9f67a3cca574ebd7a320834fc
dyi919 <ehddjs13579@gmail.com>
2023-03-12 15:27:55 +0900

    [chore] remove unnecessary codes

--- sites/test-site/src/App.tsx
+++ sites/test-site/src/App.tsx
@@ -2,9 +2,11 @@ import './App.css'
 import { SlotMachine } from 'react-slot-machine';
 
 function App() {
+  const textData = [...Array(15)].map(_ => `Data #${String(Math.round(Math.random() * 30))}`);
+
   return (
     <div className="App">
-      <SlotMachine initialText={'Click Me!'} textData={['1', '2', '3']} />
+      <SlotMachine initialText={'Click Me!'} textData={textData} />
     </div>
   )
 }
